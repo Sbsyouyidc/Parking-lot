86d6d9c7bbdd4585586cdc6c1f0265c57da75190
adadadad <2768826285@qq.com>
2023-02-26 13:59:20 +0800

    update

---
+++ src/components/Layout/index.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang='ts'>
+import {ref , reactive} from 'vue'
+
+</script>
+
+<template>
+<div class='Layout'>
+    dd
+</div>
+</template>
+<style lang=less scoped>
+
+</style>
